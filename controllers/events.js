fd6f7fdffdfbaa810762660b152dc6cd34d96fc8
Victor Gonzalez <dazedbutamused@gmail.com>
2019-10-10 14:35:32 -0700

    fixed routes and controllers

--- controllers/events.js
+++ controllers/events.js
@@ -3,50 +3,47 @@ var Event = require('../models/event');
 module.exports = {
     create,
     index,
-    // delete,
+    detail,
+    update,
+    delete: deleteOne,
 };
-
-// async function create(req, res) {
-//     try {
-//         await Event.create(req.body);
-//     } catch (err) {
-//         res.json({err});
-//     }
-// }
-
-
-// function show(req, res) {
-//   req.body.userId = req.user.id;
-//   event.findById(req.params.id, function(err, event) {
-//     console.log(items);
-//     res.render("items/show", {
-//       items,
-//       user: req.user,
-//       name: req.query.name
-//     });
-//   });
-// }
-
-// function index() {
-//   return fetch('/api/events/').then(res => res.json());
-// }
 
 async function index(req, res) {
   const event = await Event.find({});
+  res.status(200).json(event);
+}
 
-  res.json(event);
+async function detail(req, res) {
+  const event = await Event.findById(req.params.id);
+  res.status(200).json(event);
 }
 
 
-function create(req, res) {
-    const event = new Event(req.body);
-    try {
-      event.save();
-    } catch (err) {
-      // Probably a duplicate email
-      res.status(400).json(err);
-    }
-  }
+async function create(req, res) {
+  const event = await Event.create(req.body);
+  res.status(201).json(event);
+}
+
+async function update(req, res) {
+  const updateEvent = await Event.findByIdAndUpdate(req.params.id, req.body, {new: true});
+  res.status(200).json(updateEvent);
+}
+
+async function deleteOne(req, res) {
+  const deleteEvent = await Event.findByIdAndRemove(req.params.id);
+  res.status(200).json(deleteEvent);
+}
+
+
+// function create(req, res) {
+//     const event = new Event(req.body);
+//     try {
+//       event.save();
+//     } catch (err) {
+//       // Probably a duplicate email
+//       res.status(400).json(err);
+//     }
+//   }
 // async function create(req, res) {
 //     const event = new Event(req.body);
 //     try {
--- routes/api/events.js
+++ routes/api/events.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const eventsCtrl = require('../../controllers/events');
 
-router.post('/eventadd', eventsCtrl.create);
 router.get('/', eventsCtrl.index);
+router.get('/:id', eventsCtrl.detail);
+router.post('/', eventsCtrl.create);
+router.put('/:id', eventsCtrl.update);
+router.delete('/:id', eventsCtrl.delete);
 
 // router.use(require('../../config/auth');
 // router.post('/eventadd', checkAuth, eventsCtrl.create);
